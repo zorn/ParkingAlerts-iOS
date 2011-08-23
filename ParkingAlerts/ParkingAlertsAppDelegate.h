3775f4b708f5962958eb3991f29a9bd0599d6444
Mike Zornek <mike@clickablebliss.com>
2011-08-23 13:26:16 -0400

    Removed default core data stack and added MagicalRecord to project.

--- ParkingAlerts/ParkingAlertsAppDelegate.h
+++ ParkingAlerts/ParkingAlertsAppDelegate.h
@@ -13,11 +13,5 @@
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
 
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
 
 @end
